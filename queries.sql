d7ab05e92597c25ac7b4180affbecfa95438a8c1
Vivienne Zhu <vivienne82394@gmail.com>
2017-02-08 16:01:49 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,3 @@
 UPDATE `customers` SET `FirstName`='Amy' WHERE `FirstName`='Sophia'
-DELETE FROM `customers` WHERE ``LastName`='Jacob'
+DELETE FROM `customers` WHERE `LastName`='Jacob'
 SELECT `LastName`, `FirstName`, `Phone` FROM `customers` WHERE `LastName`='Levy' 
